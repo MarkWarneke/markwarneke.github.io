af8a83e2d04f21aa86ccdbfe4a4b915682985594
mark-mit-k <warneke.mark@gmail.com>
2019-08-15 22:07:46 +0200

    add two scroll up buttons

--- js/main.js
+++ js/main.js
@@ -4,9 +4,9 @@ var main = {
   bigImgEl: null,
   numImgs: null,
 
-  init: function() {
+  init: function () {
     // Shorten the navbar after scrolling a little bit down
-    $(window).scroll(function() {
+    $(window).scroll(function () {
       if ($(".navbar").offset().top > 50) {
         $(".navbar").addClass("top-nav-short");
         $(".navbar-custom .avatar-container").fadeOut(500);
@@ -17,17 +17,17 @@ var main = {
     });
 
     // On mobile, hide the avatar when expanding the navbar menu
-    $("#main-navbar").on("show.bs.collapse", function() {
+    $("#main-navbar").on("show.bs.collapse", function () {
       $(".navbar").addClass("top-nav-expanded");
     });
-    $("#main-navbar").on("hidden.bs.collapse", function() {
+    $("#main-navbar").on("hidden.bs.collapse", function () {
       $(".navbar").removeClass("top-nav-expanded");
     });
 
     // On mobile, when clicking on a multi-level navbar menu, show the child links
-    $("#main-navbar").on("click", ".navlinks-parent", function(e) {
+    $("#main-navbar").on("click", ".navlinks-parent", function (e) {
       var target = e.target;
-      $.each($(".navlinks-parent"), function(key, value) {
+      $.each($(".navlinks-parent"), function (key, value) {
         if (value == target) {
           $(value)
             .parent()
@@ -49,20 +49,20 @@ var main = {
       navbar.append(fakeMenuHtml);
       var fakeMenu = $(".fake-menu");
 
-      $.each(menus, function(i) {
+      $.each(menus, function (i) {
         var parent = $(menus[i]).find(".navlinks-parent");
         var children = $(menus[i]).find(".navlinks-children a");
         var words = [];
-        $.each(children, function(idx, el) {
+        $.each(children, function (idx, el) {
           words = words.concat(
             $(el)
-              .text()
-              .trim()
-              .split(/\s+/)
+            .text()
+            .trim()
+            .split(/\s+/)
           );
         });
         var maxwidth = 0;
-        $.each(words, function(id, word) {
+        $.each(words, function (id, word) {
           fakeMenu.html("<a>" + word + "</a>");
           var width = fakeMenu.width();
           if (width > maxwidth) {
@@ -79,7 +79,7 @@ var main = {
     main.initImgs();
   },
 
-  initImgs: function() {
+  initImgs: function () {
     // If the page was large images to randomly select from, choose an image
     if ($("#header-big-imgs").length > 0) {
       main.bigImgEl = $("#header-big-imgs");
@@ -93,7 +93,7 @@ var main = {
       main.setImg(src, desc);
 
       // For better UX, prefetch the next image so that it will already be loaded when we want to show it
-      var getNextImg = function() {
+      var getNextImg = function () {
         var imgInfo = main.getImgInfo();
         var src = imgInfo.src;
         var desc = imgInfo.desc;
@@ -102,18 +102,18 @@ var main = {
         prefetchImg.src = src;
         // if I want to do something once the image is ready: `prefetchImg.onload = function(){}`
 
-        setTimeout(function() {
+        setTimeout(function () {
           var img = $("<div></div>")
             .addClass("big-img-transition")
             .css("background-image", "url(" + src + ")");
           $(".intro-header.big-img").prepend(img);
-          setTimeout(function() {
+          setTimeout(function () {
             img.css("opacity", "1");
           }, 50);
 
           // after the animation of fading in the new image is done, prefetch the next one
           //img.one("transitioned webkitTransitionEnd oTransitionEnd MSTransitionEnd", function(){
-          setTimeout(function() {
+          setTimeout(function () {
             main.setImg(src, desc);
             img.remove();
             getNextImg();
@@ -129,7 +129,7 @@ var main = {
     }
   },
 
-  getImgInfo: function() {
+  getImgInfo: function () {
     var randNum = Math.floor(Math.random() * main.numImgs + 1);
     var src = main.bigImgEl.attr("data-img-src-" + randNum);
     var desc = main.bigImgEl.attr("data-img-desc-" + randNum);
@@ -140,7 +140,7 @@ var main = {
     };
   },
 
-  setImg: function(src, desc) {
+  setImg: function (src, desc) {
     $(".intro-header.big-img").css("background-image", "url(" + src + ")");
     if (typeof desc !== typeof undefined && desc !== false) {
       $(".img-desc")
@@ -157,7 +157,7 @@ var main = {
 document.addEventListener("DOMContentLoaded", main.init);
 
 // When the user scrolls the page, execute myFunction
-window.onscroll = function() {
+window.onscroll = function () {
   myFunction();
 };
 
@@ -171,7 +171,9 @@ function myFunction() {
 }
 
 // When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function() {scrollFunction()};
+window.onscroll = function () {
+  scrollFunction()
+};
 
 function scrollFunction() {
   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
@@ -186,3 +188,19 @@ function topFunction() {
   document.body.scrollTop = 0; // For Safari
   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
+
+$(document).ready(function () {
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 100) {
+      $('#scroll').fadeIn();
+    } else {
+      $('#scroll').fadeOut();
+    }
+  });
+  $('#scroll').click(function () {
+    $("html, body").animate({
+      scrollTop: 0
+    }, 600);
+    return false;
+  });
+});
