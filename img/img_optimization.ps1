b659a7453858a4248f31c484914678751ae20f8b
Mark Warneke <mark.warneke@microsoft.com>
2020-01-06 12:40:08 +0100

    optimize images

--- img/img_optimization.ps1
+++ img/img_optimization.ps1
@@ -1,16 +1,19 @@
 param(
     $Path = (Join-Path $PSScriptRoot "original"),
-    $imageRootPath = $PSScriptRoot # expected same as location of script
+    $imageRootPath = $PSScriptRoot, # expected same as location of script,
+    $oldExtension = "*.jpg",
+    $newExtension = ".jpeg",
+    $size = "1920x780"
 )
 
 $null = Test-Path -Path $Path
 
-$Images = Get-ChildItem -Path $Path -Recurse -Include "*.jpg"
+$Images = Get-ChildItem -Path $Path -Recurse -Include $oldExtension
 
 foreach ($Image in $Images) {
     
     $extension = [IO.Path]::GetExtension($images[0]) 
-    $newImage = $image.Name -replace $extension , ".jpeg"
+    $newImage = $image.Name -replace $extension , $newExtension
     
-    magick $Image -quality 85 -strip -resize 1920x780 $newImage
+    magick $Image -quality 85 -strip -resize $size $newImage
 }
